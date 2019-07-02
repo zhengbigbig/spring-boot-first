498caa93b0830232f229fa8d4f1d416a99500b23
zhengbigbig <780357902@qq.com>
2019-07-02 13:57:53 +0800

    template

---
+++ src/main/java/item/ItemController.java
@@ -0,0 +1,11 @@
+package item;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ItemController {
+    // 成员变量
+    // 公共状态
+
+    // API操作： GET,PUT,POST,DELETE
+}
